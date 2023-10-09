eeb9ca0554e975dccfce61e26121d1fe060300ee
vorcha100 <85402918+vorcha100@users.noreply.github.com>
2023-10-09 13:17:32 +0200

    dataTypes

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int myFirstVariable;
+        myFirstVariable = 5;
+
+        System.out.println(myFirstVariable);
+        System.out.println("myFirstVariable");
+
+        String name = "Slobodan";
+        String textToPrint = "Favorite number of " + name + " is: " + myFirstVariable;
+        System.out.println(textToPrint);
+    }
+}
---
+++ src/main/java/MyString.java
@@ -0,0 +1,24 @@
+public class MyString {
+    public static void main(String[] args) {
+        String hello = "Hello Slobodan!";
+        System.out.println(hello);
+
+        int stringLength = hello.length();
+        System.out.println(stringLength);
+
+        String helloUp = hello.toUpperCase();
+        System.out.println(helloUp);
+
+        String helloLow = hello.toLowerCase();
+        System.out.println(helloLow);
+
+        String addStrings = helloLow + helloUp;
+        System.out.println(addStrings);
+
+        String stringWithInt = hello + ", this string lenght is: " + stringLength;
+        System.out.println(stringWithInt);
+
+        String movie = "My favorite movie is \n\"Home Alone\"";
+        System.out.println(movie);
+    }
+}
